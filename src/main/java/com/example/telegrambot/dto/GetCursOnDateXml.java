c633b12837e092e0ab26c1cb6349d988e129d23e
Sergey-whiteserg88 <whiteserg88@yandex.ru>
2023-04-12 21:26:27 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrambot/dto/GetCursOnDateXml.java
@@ -0,0 +1,20 @@
+package com.example.telegrambot.dto;
+
+
+import lombok.Data;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "GetCursOnDateXML", namespace = "http://web.cbr.ru/")
+@Data
+public class GetCursOnDateXml {
+
+    @XmlElement(name = "On_date", required = true, namespace = "http://web.cbr.ru/")
+    protected XMLGregorianCalendar onDate;
+
+}
